faf07297d307b4c9620f82fb19a4d81537095f50
j.tenbroeke <johantenbroeke@johans-mbp.lan>
2013-11-27 21:45:29 +0100

    added example implementation to unimplemented exception in spiral baseclass

--- blackstripesMK2/machine_motion/BaseSpiral.py
+++ blackstripesMK2/machine_motion/BaseSpiral.py
@@ -4,7 +4,19 @@ from machine_settings import *
 class BaseSpiral:
 
     def configure(self):
-        raise Exception("please implement configure")
+        msg = """
+
+            please implement configure:
+
+            def configure(self):
+                self.radius = 489.0
+                self.spiral_start = (989.0 ,500.0)
+                self.bounding_radius = 465
+                self.signature_pos = (790,917)
+                self.max_cycles = 119
+
+            """
+        raise Exception(msg)
 
     def __init__(self,s,nib_size_mm=0.5):
         self.configure()
@@ -93,5 +105,8 @@ class BaseSpiral:
         self.s.releaseMoveMode()
         
 if __name__ == "__main__":
-    pass
+    from Model import Blackstripes_MKII
+    m = Blackstripes_MKII()
+    BaseSpiral(m,MARKER_NIB_SIZE_MM)
+
     
