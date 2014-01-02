2df3e4ef5ae507f16454cb75faf004869b46cc09
johantenbroeke <johan@fullscreen.nl>
2014-01-02 12:37:03 +0100

    experimental cropping previews

--- blackstripesMK2/webbased_preview/gen_webbased_preview.py
+++ blackstripesMK2/webbased_preview/gen_webbased_preview.py
@@ -1,5 +1,81 @@
 import numpy
 import Image
+import os
+
+OUPUT_DIR = "output/"
+
+class OutputFolder:
+    def __init__(self):
+        try:
+            os.makedirs(OUPUT_DIR)
+        except:
+            print "folder OK"
+
+
+class Cropper:
+
+    W = 500
+    H = 500
+
+    def __init__(self,image_name,imid):
+
+        self.imid = imid
+
+        im = Image.open(image_name)
+        w,h = im.size
+
+        crops = None
+
+        if w < h:
+
+            zoom = int(round(w * 0.25))
+            zoom_2 = int(round(w * 0.125))
+            offset_1 = 0
+            offset_2 = int(round((h-w)/2.0))
+            offset_3 = int(round(h-w))
+            crops = [
+                (0,offset_1,w,w+offset_1),
+                (0,offset_2,w,w+offset_2),
+                (0,offset_3,w,w+offset_3),
+                #zoomed
+                (zoom,offset_1+zoom,w-zoom,w+offset_1-zoom),
+                (zoom,offset_2+zoom,w-zoom,w+offset_2-zoom),
+                (zoom,offset_3+zoom,w-zoom,w+offset_3-zoom),
+
+                (zoom_2,offset_1+zoom_2,w-zoom_2,w+offset_1-zoom_2),
+                (zoom_2,offset_2+zoom_2,w-zoom_2,w+offset_2-zoom_2),
+                (zoom_2,offset_3+zoom_2,w-zoom_2,w+offset_3-zoom_2)
+            ]
+
+            
+        else:
+
+            zoom = int(round(h * 0.25))
+            zoom_2 = int(round(h * 0.125))
+            offset_1 = 0
+            offset_2 = int(round((w-h)/2.0))
+            offset_3 = int(round(w-h))
+            crops = [
+                (offset_1,0,h+offset_1,h),
+                (offset_2,0,h+offset_2,h),
+                (offset_3,0,h+offset_3,h),
+                #zoomed
+                (offset_1+zoom,zoom,h+offset_1-zoom,h-zoom),
+                (offset_2+zoom,zoom,h+offset_2-zoom,h-zoom),
+                (offset_3+zoom,zoom,h+offset_3-zoom,h-zoom),
+
+                (offset_1+zoom_2,zoom_2,h+offset_1-zoom_2,h-zoom_2),
+                (offset_2+zoom_2,zoom_2,h+offset_2-zoom_2,h-zoom_2),
+                (offset_3+zoom_2,zoom_2,h+offset_3-zoom_2,h-zoom_2)
+            ]
+
+        cr_count = 0
+        for cr in crops:
+            crim = im.crop(cr)
+            _s = int(self.W),int(self.H)
+            crim = crim.resize(_s,Image.BICUBIC)
+            crim.save(OUPUT_DIR+self.imid+str(cr_count)+".jpg")
+            cr_count += 1
 
 
 class WebBasedPreviews:
@@ -40,6 +116,8 @@ class WebBasedPreviews:
                 levels.append(c+cs*3)
 
                 my_level_data = []
+
+                print str(levels).strip("[]").replace(", ","_")
 
                 for l in levels:
                     my_level_data.append(self.level_data[l])
@@ -82,11 +160,14 @@ class WebBasedPreviews:
 
                 a = numpy.uint8(a)
                 t = Image.fromarray(a)
-                t.save(str(c)+"_"+str(cs)+".png")
+                t.save(OUPUT_DIR+str(c)+"_"+str(cs)+".png")
 
 
 if __name__ == "__main__":
-    WebBasedPreviews("miles.jpg")
+    OutputFolder()
+    #Cropper("photo.jpg","one")
+    Cropper("cliff-burton.jpg","image_crop")
+    WebBasedPreviews(OUPUT_DIR+"image_crop5.jpg")
 
 
 
