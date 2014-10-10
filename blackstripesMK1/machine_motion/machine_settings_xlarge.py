045ea50b11c436570f3da0d4b9b003e1e1f30d64
johantenbroeke <johan@fullscreen.nl>
2014-10-10 15:57:57 +0200

    added xlarge settings

---
+++ blackstripesMK1/machine_motion/machine_settings_xlarge.py
@@ -0,0 +1,23 @@
+import os
+print "## settings: "+os.path.basename(__file__)+" ##"
+
+DOTSIZE = 1.5
+TRANSITION_DOTSIZE = 5
+LEFT_STEPPER_POS = (0.0,0.0)
+RIGHT_STEPPER_POS = (2915.0,0.0)
+CANVAS_SIZE = (1500,1500)
+PADDING = 300
+LINE_SPACING = 12.5 * (1000.0/1500.0)
+CURVED_ITERATIONS = 825
+STRAIGHT_ITERATIONS = 250
+LINE_ALPHA = 255
+
+STEPS_PER_MM_PREVIEW = 2.5 
+STEPS_PER_MM = 42.5532
+Z_STEPS_PER_MM = 2 
+
+###### globals #####
+g_Offset = (RIGHT_STEPPER_POS[0] - CANVAS_SIZE[0]) / 2
+g_MachineWidth = int(RIGHT_STEPPER_POS[0])
+#g_NullPosition = lengthFromPos(750,750)
+g_NullPosition = (1580,1580)
